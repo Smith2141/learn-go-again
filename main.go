60366dc66d2914d82a30421b9123e4e44a5f6288
Maxim_RZ <berezovski-ma@mttech.moscow>
2026-08-28 16:12:09 +0300

    go_learn iota

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+const (
+	Black = iota
+	Gray
+	White
+)
+
+// счётчик обнуляется
+const (
+	Yellow = iota
+	Red
+	Green = iota // это присваивание не обнулит iota
+	Blue
+)
+
 func main() {
-	// определите переменные ver, id, pi
-	var (
-		ver = 0.001
-		id int
-		pi = 3.1415
-	)
-
-	var verV string = fmt.Sprintf("v%g", ver)
-
-	fmt.Println("ver =", verV, "id =", id, "pi =", pi)
+	fmt.Println(Black, Gray, White)
+	fmt.Println(Yellow, Red, Green, Blue)
 }
