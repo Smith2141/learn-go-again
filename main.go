50ee3e2472192bfac244ba3331826b2092633c8b
maxim <acer.laptop>
2026-08-31 22:03:06 +0300

    go_learn FizzBuzz

--- main.go
+++ main.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// создаём массив
-	array := [3]int{1, 2, 3}
-	// итерируемся
-	for arrayIndex, arrayValue := range array {
-		fmt.Printf("array[%d]: %d\n", arrayIndex, arrayValue)
+
+	for j := 1; j <= 100; j++ {
+		var fizz, buzz, sum int
+
+		// fmt.Print(j, ":")
+		if j%3 == 0 {
+			fizz = 1
+		}
+		if j%5 == 0 {
+			buzz = 2
+		}
+		sum = fizz + buzz
+		// fmt.Println("f b s", fizz, buzz, sum)
+
+		switch sum {
+		case 3:
+			fmt.Println("FizzBuzz")
+		case 2:
+			fmt.Println("Buzz")
+		case 1:
+			fmt.Println("Fizz")
+		default:
+			fmt.Println(j)
+		}
 	}
 }
