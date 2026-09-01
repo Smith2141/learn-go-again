b7a0a82109cd0a3368819e8ff01a5f79f1e0673f
Maxim_RZ <berezovski-ma@mttech.moscow>
2026-09-01 10:09:12 +0300

    go_learn pointer to pointer example

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func main() {
 
-	p := new(int)
-	fmt.Println("Value:", *p) // Value: 0 - значение по умолчанию
-	*p = 8                    // изменяем значение
-	fmt.Println("Value:", *p) // Value: 8
+	var a int = 9
+
+	var p_a *int = &a // p_a - указатель на переменную a
+
+	// обращаемся к указателю p_a
+	fmt.Println("Value of a:", *p_a)  // Value of a: 9
+	fmt.Println("Address of a:", p_a) // Address of a: 0xc000010100
+
+	var p_p_a **int = &p_a // p_p_a - указатель на переменную p_a
+
+	// обращаемся к указателю p_a
+	fmt.Println("Value of p_a:", *p_p_a)  // Value of p_a: 0xc000010100
+	fmt.Println("Address of p_a:", p_p_a) // Address of p_a: 0xc000076040
 }
