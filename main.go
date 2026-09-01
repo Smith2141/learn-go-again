b8231371f0f4e44df4e7da870b8aff3b86dd0696
maxim <acer.laptop>
2026-09-01 21:37:29 +0300

    pointer_task result commit

--- main.go
+++ main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    // Получаем читателя пользовательского ввода
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Println("Interaction counter")
+	// Получаем читателя пользовательского ввода
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Interaction counter")
 
-    cnt := 0
-    for {
-        fmt.Print("-> ")
-                // Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку 
-        _, err := reader.ReadString('\n')
-        if err != nil {
-            panic(err)
-        }
+	cnt := 0
+	for {
+		fmt.Print("-> ")
+		// Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку
+		_, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
 
-        f(&cnt)
+		f(&cnt)
 
-        fmt.Printf("User input %d lines\n", cnt)
-    }
+		fmt.Printf("User input %d lines\n", cnt)
+	}
+
+}
+
+func f(p *int) {
+	*p++
 }
