5928e0a2480b291c97790a459eb4746407ecfb85
Maxim_RZ <berezovski-ma@mttech.moscow>
2026-09-01 09:56:06 +0300

    go_learn Pointer example

--- main.go
+++ main.go
@@ -3,29 +3,8 @@ package main
 import "fmt"
 
 func main() {
-
-	for j := 1; j <= 100; j++ {
-		var fizz, buzz, sum int
-
-		// fmt.Print(j, ":")
-		if j%3 == 0 {
-			fizz = 1
-		}
-		if j%5 == 0 {
-			buzz = 2
-		}
-		sum = fizz + buzz
-		// fmt.Println("f b s", fizz, buzz, sum)
-
-		switch sum {
-		case 3:
-			fmt.Println("FizzBuzz")
-		case 2:
-			fmt.Println("Buzz")
-		case 1:
-			fmt.Println("Fizz")
-		default:
-			fmt.Println(j)
-		}
-	}
+	var x int = 4  // определяем переменную
+	var p *int     // определяем указатель
+	p = &x         // указатель получает адрес переменной
+	fmt.Println(p) // значение самого указателя - адрес переменной x
 }
