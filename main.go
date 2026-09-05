2c9b2a7dac78e114d081886540e289530861dd7d
maxim <acer.laptop>
2026-09-05 16:33:46 +0300

    pointer_task Add slice sort demo

--- main.go
+++ main.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"reflect" // для сравнения срезов
+	"sort"
 )
 
 func main() {
-	// Получаем читателя пользовательского ввода
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Interaction counter")
 
-	cnt := 0
-	for {
-		fmt.Print("-> ")
-		// Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку
-		_, err := reader.ReadString('\n')
-		if err != nil {
-			panic(err)
-		}
+	slice1 := []int{1, 2, 3, 4}
+	slice2 := []string{"Tom", "Bob", "Sam"}
+	slice3 := []int{1, 2, 3}
+	slice4 := []int{1, 2, 4, 3}
+	sort.Ints(slice4)
 
-		f(&cnt)
-
-		fmt.Printf("User input %d lines\n", cnt)
-	}
-
-}
-
-func f(p *int) {
-	*p++
+	fmt.Println("slice1 == slice2:", reflect.DeepEqual(slice1, slice2)) // false
+	fmt.Println("slice1 == slice3:", reflect.DeepEqual(slice1, slice3)) // false
+	fmt.Println("slice1 == slice4:", reflect.DeepEqual(slice1, slice4)) // true
 }
