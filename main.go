9f3dbedd26767123d2e789ca3078c065e80c958d
maxim <acer.laptop>
2026-09-07 22:32:20 +0300

    pointer_task Add map demo

--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"reflect" // для сравнения срезов
-	"sort"
 )
 
 func main() {
-
-	slice1 := []int{1, 2, 3, 4}
-	slice2 := []string{"Tom", "Bob", "Sam"}
-	slice3 := []int{1, 2, 3}
-	slice4 := []int{1, 2, 4, 3}
-	sort.Ints(slice4)
-
-	fmt.Println("slice1 == slice2:", reflect.DeepEqual(slice1, slice2)) // false
-	fmt.Println("slice1 == slice3:", reflect.DeepEqual(slice1, slice3)) // false
-	fmt.Println("slice1 == slice4:", reflect.DeepEqual(slice1, slice4)) // true
+	m := make(map[string]string) // создаём map — о применении функции make для создания переменных типа map будет рассказано ниже
+	m["foo"] = "bar"             // заполняем парами «ключ-значение»
+	m["ping"] = "pong"
+	fmt.Println(m) // печатаем
 }
