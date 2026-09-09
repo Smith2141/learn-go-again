5d5e36cdaa9e067f8059915a51e083e866e5f219
maxim <acer.laptop>
2026-09-09 10:00:05 +0300

    pointer_task Задача по мапам - заказы

--- main.go
+++ main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	// предложение
-	sentence := "Πολύ λίγα πράγματα συμβαίνουν στο σωστό χρόνο, και τα υπόλοιπα δεν συμβαίνουν καθόλου"
-	// инициализируем map
-	// ключами будут знаки, а значениями — количество вхождений
-	frequency := make(map[rune]int)
-	// пройдёмся по знакам в предложении
-	for _, v := range sentence {
-		frequency[v]++ // встреченному знаку увеличиваем частоту
+	var products = map[string]int{
+		"хлеб":     50,
+		"молоко":   100,
+		"масло":    200,
+		"колбаса":  500,
+		"соль":     20,
+		"огурцы":   200,
+		"сыр":      600,
+		"ветчина":  700,
+		"буженина": 900,
+		"помидоры": 250,
+		"рыба":     300,
+		"хамон":    1500,
 	}
-	// печатаем
-	for k, v := range frequency {
-		fmt.Printf("Знак %c встречается %d раз \n", k, v)
+
+	for p := range products {
+		if products[p] > 500 {
+			fmt.Println(p)
+		}
 	}
+
+	// delimiter
+	delimiter := strings.Repeat("-", 50)
+	fmt.Println(delimiter)
+
+	var order = []string{"хлеб", "буженина", "сыр", "огурцы", "чай"}
+
+	var order_sum int = 0
+	for _, item := range order {
+		if price, ok := products[item]; ok {
+			// добавить форматирование !!!
+			fmt.Println(products[item], price)
+			order_sum += price
+		}
+	}
+
+	fmt.Println(delimiter)
+	fmt.Println("Order total: ", order_sum)
 }
