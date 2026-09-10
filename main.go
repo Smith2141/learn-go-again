504329966849f30345c2a35df060f6f6f3bb0910
maxim <acer.laptop>
2026-09-10 11:40:09 +0300

    master Задача по мапам - фильтр начало

--- main.go
+++ main.go
@@ -2,46 +2,39 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var products = map[string]int{
-		"хлеб":     50,
-		"молоко":   100,
-		"масло":    200,
-		"колбаса":  500,
-		"соль":     20,
-		"огурцы":   200,
-		"сыр":      600,
-		"ветчина":  700,
-		"буженина": 900,
-		"помидоры": 250,
-		"рыба":     300,
-		"хамон":    1500,
+	input := []string{
+		"cat",
+		"dog",
+		"bird",
+		"dog",
+		"parrot",
+		"cat",
 	}
 
-	for product, price := range products {
-		if price > 500 {
-			fmt.Println(product)
+	fmt.Println("total: ", RemoveDuplicates(input))
+}
+
+func RemoveDuplicates(input []string) []string {
+	var storage = map[string]string{}
+	var result []string
+
+	for i, value := range input {
+		// fmt.Println(value)
+		if _, ok := storage[value]; ok {
+			fmt.Println("is present: ", value)
+
+			// result = input[:i]
+		} else {
+			fmt.Println("is NOT present: ", value)
+			storage[value] = value
+			fmt.Println("input BEFORE: ", result)
+			result = append(result, input[i])
+			fmt.Println("input AFTER: ", result)
 		}
 	}
 
-	// delimiter
-	delimiter := strings.Repeat("-", 50)
-	fmt.Println(delimiter)
-
-	var order = []string{"хлеб", "буженина", "сыр", "огурцы"}
-
-	var total int = 0
-	for _, item := range order {
-		if price, ok := products[item]; ok {
-			// добавить форматирование !!!
-			fmt.Printf("Товар: %s, стоимость %d\n", item, price)
-			total += price
-		}
-	}
-
-	fmt.Println(delimiter)
-	fmt.Println("Стоимость заказа ", total)
+	return result
 }
