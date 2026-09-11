42435e1b905d0ed2af065b967e5cf0d0b732f2a7
maxim <acer.laptop>
2026-09-11 22:33:17 +0300

    master Add structure json opt 2

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 	"log"
+	"time"
 )
 
 type Person struct {
-	Name        string `json:"Имя"`
-	Email       string `json:"Почта"`
+	Name        string    `json:"Имя"`
+	Email       string    `json:"Почта"`
 	DateOfBirth time.Time `json:"-"`
 }
 
 func main() {
 
 	man := Person{
-		Name:  "Alex",
-		Email: "alex@yandex.ru",
-		DateOfBirth:time.Now(),
+		Name:        "Alex",
+		Email:       "alex@yandex.ru",
+		DateOfBirth: time.Now(),
 	}
 
 	result, err := json.Marshal(man)
 
 	if err != nil {
-        log.Fatalln("unable marshal to json")
+		log.Fatalln("unable marshal to json")
 	}
 
-	fmt.Printf("Man %v\n",string(result))
+	fmt.Printf("Man %v\n", string(result))
 }
